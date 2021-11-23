9f8d3de5cdd4dd4c4898021f68c0a6e655b06e51
Richard Hawkins <richard.hawkins@revature.com>
2021-11-23 12:16:32 -0500

    Collections and Functions

---
+++ wk2/ComplexDataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿static void Main()
+{
+    int newInt = 8;
+    int[] intArray = new int[] {3,4,5};
+    int[] secondIntArray = new int[5];
+    int[] arrayone = new int[] {3,4,5};
+    int[] arraytwo = {1,2,3,4,5,6,7};
+
+    Console.WriteLine("newInt = " + newInt );
+    Console.WriteLine("intArray = " + intArray[1]);
+    Console.WriteLine("arrayone = " + arrayone[1]);
+    Console.WriteLine("arraytwo = " + arraytwo[1]);
+
+    Console.WriteLine("printing arrayone");
+    PrintArray(arrayone);
+    Console.WriteLine("print arraytwo");
+    PrintArray(arraytwo);
+}
+
+static void PrintArray( int[] Array)
+{
+    for( int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine(Array[i]);
+    }
+}
+
+
+Main();
---
+++ wk2/Functions/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+static void Main()
+{
+
+    for (int counterForTheForLoop = 0; counterForTheForLoop < 5; counterForTheForLoop++)
+    {
+        int returnedValueFromSayHello = SayHello();
+        Console.WriteLine(returnedValueFromSayHello);
+    }
+
+    int a,b;
+    a=2;
+    b=4;
+
+    int c;
+
+    c = NewFunc(a,b);  //call the function, send variables
+    Console.WriteLine(c);
+
+    c = NewFunc(5,6); //call the function, send values
+    Console.WriteLine(c);
+
+    Console.WriteLine(NewFunc(1,4));   //return function value to output. 
+
+    c = Multiply(5,6);
+    Console.WriteLine(c);
+}
+
+static int NewFunc(int x, int y)
+{
+ int z;
+ z = x+y;   
+ return z;
+
+ //return (x+y);
+}
+
+static int Multiply(int x, int y)
+{
+ int z;
+ z = x*y;   
+ return z;
+}
+
+static int SayHello()
+{
+    Console.WriteLine("Hi There!");
+    int j = NewFunc(7,4);
+    return j;
+}
+
+Main();
