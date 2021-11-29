34e2033720f66085b24a33b9f8f6b7b02ea85e72
Richard Hawkins <richard.hawkins@revature.com>
2021-11-29 13:00:39 -0500

    wk3, Exceptions, FileInteractions

---
+++ wk3/Exceptions/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Exceptions:");
+
+bool loop = false;
+string? myString;
+
+do
+{
+    Console.WriteLine("Enter a string:");
+    myString = Console.ReadLine();
+
+    try
+    {
+        Console.WriteLine(myString[0]);
+        loop = false;
+    }
+    catch (IndexOutOfRangeException e)
+    {
+        Console.WriteLine("Uh oh, you enterd no string!");
+        loop = true;
+    }
+    finally
+    {
+        Console.WriteLine("Yup, it's saved");
+    }
+
+
+    // bool testOne = (myString == null);
+    // bool testTwo = (a != b);
+    // bool result = testOne || testTwo;
+}
+while( loop );
+
+
+
+
+
+
+
+
+// int[] numbers = {0,1,2,3,4,5,6};
+// Console.WriteLine("Enter and index between 0 and 6");
+// int index = int.Parse(Console.ReadLine());
+
+// try
+// {
+//     Console.WriteLine(numbers[index]);
+// }
+// catch (IndexOutOfRangeException e)
+// {
+//     Console.WriteLine("requested index not in array");
+//     Console.WriteLine($"{e.GetType().Name}: {index} is outside the range of the array");
+    
+// }
---
+++ wk3/FileInteraction/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System;
+
+
+string path = "./testFile.txt";
+
+// /c/Users/Richard/revature/trainer-code/wk3/FileInteraction/testFile.txt
+
+
+string[] text = {"hi", "hello", "How's it going" };
+
+File.AppendAllLines(path, text);
+
+
+
+
+
+
+// StreamReader reader = new StreamReader(path);
+// while(reader.Peek() != -1)
+// {
+//     Console.WriteLine(reader.ReadLine());
+// }
+// Console.WriteLine("End of file reached");
+// reader.Close();
+
+
+
+
+
+// StreamWriter writer = new StreamWriter(path);
+// writer.WriteLine("This is a new line.");
+// writer.Close();
