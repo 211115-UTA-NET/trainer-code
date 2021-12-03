771936a8f69ed3362cd6b0c400382ba1eee138b2
Richard Hawkins <richard.hawkins@revature.com>
2021-12-03 10:34:07 -0500

    Interfaces

---
+++ wk3/Interfaces/Program.cs
@@ -0,0 +1,46 @@
+﻿//Interfaces are VERY similar to an abstract class.
+// an interface cannot be implimented on its own.
+// must be inherited/extended.
+
+// abstract class Abstract
+// {
+//     private string AbstractString = "I am a string.";
+
+// }
+
+
+//Interface!
+interface IAnimal
+{
+    void animalSound();
+}
+
+interface IMammal
+{
+    void warmBlooded();
+}
+
+class Pig : IAnimal , IMammal
+{
+    public void animalSound()
+    {
+        Console.WriteLine("Oink!");
+    }
+
+    public void warmBlooded()
+    {
+        Console.WriteLine("Toasty!");
+    }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        Pig myPig = new Pig();
+        myPig.animalSound();
+        myPig.warmBlooded();
+
+    }
+}
