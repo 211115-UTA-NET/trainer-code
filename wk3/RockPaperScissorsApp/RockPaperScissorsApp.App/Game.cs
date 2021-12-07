b5c8207ac9fd46aaa6efd45460e9187668b4b0b7
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-07 14:36:26 -0600

    add rps intro

---
+++ wk3/RockPaperScissorsApp/RockPaperScissorsApp.App/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace RockPaperScissorsApp.App
+{
+    public class Game
+    {
+        public string Summary
+        {
+            get
+            {
+                return "(not implemented yet)";
+            }
+        }
+
+        public void PlayRound()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wk3/RockPaperScissorsApp/RockPaperScissorsApp.App/Program.cs
@@ -0,0 +1,44 @@
+﻿// app to play rock-paper-scissors with the computer
+
+// required features:
+// - play multiple rounds
+// - get a summary/record of all the rounds played so far
+
+// stretch goal features:
+// - persistence (save data somehow, it will remember past game history) (and clear history)
+// - play more complex variants of RPS (like RPS+lizard+spock)
+// - logging (to help with debugging the app if something goes wrong)
+// - some way to have more than 2 players at a time
+// - support both player vs player and player vs computer
+// - difficulty settings for the computer (remembers your moves and tries to predict)
+
+// - in general, we want to write something simple
+//    but in a way that allows for extending it / generalizing it in the future.
+
+namespace RockPaperScissorsApp.App
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to RockPaperScissors App");
+
+            var game = new Game();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Play a round? (y/n) ");
+
+                string? input = Console.ReadLine();
+
+                if (input != "y") { break; }
+
+                game.PlayRound();
+            }
+
+            string summary = game.Summary;
+            Console.WriteLine(summary);
+        }
+    }
+}
