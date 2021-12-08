175d3d6530c6c44d0707913d4e450cf8ed966f06
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-08 14:07:38 -0600

    xml customization

--- wk3/RockPaperScissorsApp/RockPaperScissorsApp.App/Record.cs
+++ wk3/RockPaperScissorsApp/RockPaperScissorsApp.App/Record.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace RockPaperScissorsApp.App
 {
@@ -14,9 +15,17 @@ namespace RockPaperScissorsApp.App
         //  a class that ONLY has a zero-arg constructor and public get set properties is sometimes
         //    called a POCO (plain old CLR object)
 
+
+        // C# has "attributes" - they don't do anything by themselves,
+        // they're there for some other code to notice them and make some change to their own behavior
+
         // ex: [ 10/7 Computer: Rock VS You: Paper => You Win! ]
+        [XmlAttribute(AttributeName = "When")]
         public DateTime Date { get; set; }
+
+        [XmlElement(ElementName = "CPUMove")]
         public string PC { get; set; }
+        [XmlElement(ElementName = "PlayerMove")]
         public string Player { get; set; }
         public string result { get; set; }
         // constructor
