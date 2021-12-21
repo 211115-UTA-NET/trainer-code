c5d557f4b07eaed6715bb987517214b2e3f47e3c
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-21 10:05:50 -0600

    map as an extension method

--- wk5/DelegatesDemo/DelegatesDemo/ExtensionMethods.cs
+++ wk5/DelegatesDemo/DelegatesDemo/ExtensionMethods.cs
@@ -20,5 +20,25 @@
         {
             return num * other * third;
         }
+
+        // extension methods are a C# syntax that let's you take static methods
+        // and seemingly attach them to already-defined data types, even builtin ones or sealed ones
+        // they're not REALLY attached to those types, it's only a matter of nice-looking syntax.
+        //   (you can only use an extension method when the static class that defines it is
+        //    in the same namespace or referenced with a using directive).
+        // extension methods are just a nice way to define helper methods
+        //    you can't do inheritance with them
+
+        // let's define something actually useful in many situations - 
+        // a method the help us transform or map a list to a new value for each element of that list
+        public static IEnumerable<T> Map<T>(this IEnumerable<T> list, Mapper<T> mapper)
+        {
+            List<T> result = new();
+            foreach (T item in list)
+            {
+                result.Add(mapper(item));
+            }
+            return result;
+        }
     }
 }
--- wk5/DelegatesDemo/DelegatesDemo/Program.cs
+++ wk5/DelegatesDemo/DelegatesDemo/Program.cs
@@ -21,8 +21,8 @@ namespace DelegatesDemo
         {
             Console.WriteLine(10.Square());
             Console.WriteLine(5.Multiply(2, 3)); // 30
-            var numbers = new List<int> { 1, 2, 3, 4 };
-            numbers = Map(numbers, delegate(int x) { return x.Square(); });
+            IEnumerable<int> numbers = new List<int> { 1, 2, 3, 4 };
+            numbers = numbers.Map(delegate(int x) { return x.Square(); });
             // calling the method - wrong
             //NumberFormatter formatter = CommaFormat();
             // referencing the method - correct
@@ -30,34 +30,15 @@ namespace DelegatesDemo
             NumberFormatter formatter = delegate (int num) { return $"{num} "; };
             PrintNumbers(numbers, formatter);
 
-            Map(new List<string> { "a", "b", "c" }, delegate (string x) { return x.ToUpper(); });
+            var list = new List<string> { "a", "b", "c" };
+            list.Map(delegate (string x) { return x.ToUpper(); });
         }
 
         // C# borrows a little from "functional programming" (as opposed to OOP)
         //    whereby functions/behavior are just another kind of data
 
-        // extension methods are a C# syntax that let's you take static methods
-        // and seemingly attach them to already-defined data types, even builtin ones or sealed ones
-        // they're not REALLY attached to those types, it's only a matter of nice-looking syntax.
-        //   (you can only use an extension method when the static class that defines it is
-        //    in the same namespace or referenced with a using directive).
-        // extension methods are just a nice way to define helper methods
-        //    you can't do inheritance with them
-
-        // let's define something actually useful in many situations - 
-        // a method the help us transform or map a list to a new value for each element of that list
-        public static List<T> Map<T>(List<T> list, Mapper<T> mapper)
-        {
-            List<T> result = new();
-            foreach (T item in list)
-            {
-                result.Add(mapper(item));
-            }
-            return result;
-        }
-
         // if you want to declare something with delegate type, you need to define that specific delegate type
-        public static void PrintNumbers(List<int> numbers, NumberFormatter formatter)
+        public static void PrintNumbers(IEnumerable<int> numbers, NumberFormatter formatter)
         {
             Console.Write("[");
             foreach (int num in numbers)
