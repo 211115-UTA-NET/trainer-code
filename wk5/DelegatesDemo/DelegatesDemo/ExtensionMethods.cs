5011f050176e007c77b16bdad43f3f8a25e638ad
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-21 10:00:42 -0600

    extension methods

---
+++ wk5/DelegatesDemo/DelegatesDemo/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegatesDemo.Extensions
+{
+    public static class ExtensionMethods
+    {
+        // not an extension method
+        // called like 'ExtensionMethods.Square(x)'
+        //public static int Square(int num)
+        //{
+        //    return num * num;
+        //}
+
+        // an extension method
+        // called like 'x.Square()'
+        public static int Square(this int num)
+        {
+            return num.Multiply(num);
+        }
+
+        public static int Multiply(this int num, int other, int third = 1)
+        {
+            return num * other * third;
+        }
+    }
+}
--- wk5/DelegatesDemo/DelegatesDemo/Program.cs
+++ wk5/DelegatesDemo/DelegatesDemo/Program.cs
@@ -1,37 +1,55 @@
-﻿namespace DelegatesDemo
+﻿using DelegatesDemo.Extensions;
+
+namespace DelegatesDemo
 {
     // this defines a new delegate type - valid values
     // are any method that has one int parameter and returns string.
     public delegate string NumberFormatter(int num);
 
-    public delegate int NumberMapper(int num);
+    public delegate T Mapper<T>(T value);
 
     // this defines a new class type- valid values are
     // any object produced by its (default) constructor
     public class Program
     {
+        public static void TryOutExtensionMethods()
+        {
+
+        }
 
         public static void Main()
         {
+            Console.WriteLine(10.Square());
+            Console.WriteLine(5.Multiply(2, 3)); // 30
             var numbers = new List<int> { 1, 2, 3, 4 };
-            numbers = Map(numbers, delegate(int x) { return x * x; });
+            numbers = Map(numbers, delegate(int x) { return x.Square(); });
             // calling the method - wrong
             //NumberFormatter formatter = CommaFormat();
             // referencing the method - correct
             //NumberFormatter formatter = CommaFormat;
             NumberFormatter formatter = delegate (int num) { return $"{num} "; };
             PrintNumbers(numbers, formatter);
+
+            Map(new List<string> { "a", "b", "c" }, delegate (string x) { return x.ToUpper(); });
         }
 
         // C# borrows a little from "functional programming" (as opposed to OOP)
         //    whereby functions/behavior are just another kind of data
 
+        // extension methods are a C# syntax that let's you take static methods
+        // and seemingly attach them to already-defined data types, even builtin ones or sealed ones
+        // they're not REALLY attached to those types, it's only a matter of nice-looking syntax.
+        //   (you can only use an extension method when the static class that defines it is
+        //    in the same namespace or referenced with a using directive).
+        // extension methods are just a nice way to define helper methods
+        //    you can't do inheritance with them
+
         // let's define something actually useful in many situations - 
         // a method the help us transform or map a list to a new value for each element of that list
-        public static List<int> Map(List<int> list, NumberMapper mapper)
+        public static List<T> Map<T>(List<T> list, Mapper<T> mapper)
         {
-            List<int> result = new();
-            foreach (int item in list)
+            List<T> result = new();
+            foreach (T item in list)
             {
                 result.Add(mapper(item));
             }
