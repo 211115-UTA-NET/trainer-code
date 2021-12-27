359ba2c1302084241950d0b1fee3e1f5c7bdeb18
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-27 15:03:25 -0600

    move aspnetcoreintro and add diagrams

---
+++ wk5.5/AspNetCoreIntro/AspNetCoreIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+// after you have a WebApplication, you need to construct its
+// request-processing pipeline using components called "middlewares".
+
+// this call adds a middleware to the pipeline.
+// each middleware runs in sequence.
+app.Run(context =>
+{
+    // the HttpContext parameter (context) gives access to all the request data
+    // and lets you modify all the response data.
+    context.Response.StatusCode = 204;
+
+    // need this line so it compiles
+    // (really this delegate should be async but we haven't done that yet)
+    return Task.CompletedTask;
+});
+
+
+// this call runs the app
+app.Run();
