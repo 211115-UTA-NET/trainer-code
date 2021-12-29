e334c3c9b2ec4acc047c6af0053c74e3020e5ed8
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-12-29 14:46:27 -0600

    implement client side with ttpclient a little

---
+++ wk5.5/RpsConsoleApp/RpsConsoleApp.UI/Dtos/SubmittedMove.cs
@@ -0,0 +1,8 @@
+﻿namespace RpsConsoleApp.UI.Dtos
+{
+    public class SubmittedMove
+    {
+        public string? PlayerName { get; set; }
+        public string? Move { get; set; }
+    }
+}
--- wk5.5/RpsConsoleApp/RpsConsoleApp.UI/Program.cs
+++ wk5.5/RpsConsoleApp/RpsConsoleApp.UI/Program.cs
@@ -17,10 +17,17 @@
  *   but in a way that allows for extending it/generalizing it in the future.
  */
 
+using System.Net.Http.Json;
+using System.Text.Json;
+using RpsConsoleApp.UI;
+using RpsConsoleApp.UI.Dtos;
+using RpsConsoleApp.UI.Exceptions;
+
 namespace RockPaperScissorsApp.App
 {
     public class Program
     {
+        public static readonly HttpClient HttpClient = new();
 
         public async static Task Main(string[] args)
         {
@@ -32,8 +39,33 @@ namespace RockPaperScissorsApp.App
                 name = Console.ReadLine();
             }
 
-            //string connectionString = await File.ReadAllTextAsync("C:/revature/richard-rps-db.txt");
-            //IRepository repository = new SqlRepository(connectionString);
+            // not great to hardcode the URL, but if you do, best that it's up in the main method
+            // and not buried somewhere not obvious, or worse,
+            // duplicated in several places.
+            // better approach would be, read it from command line args, environment variables, or maybe a file
+            Uri server = new("https://localhost:7175");
+
+            IGameService gameService = new GameService(server);
+
+            // Accept header on the request tells the server what media type you expect in the response to that request.
+            //HttpResponseMessage response = await HttpClient.GetAsync($"https://localhost:7175/api/rounds?player={name}");
+            List<Round> rounds;
+            try
+            {
+                rounds = await gameService.GetRoundsOfPlayerAsync(name);
+            }
+            catch (UnexpectedServerBehaviorException)
+            {
+                Console.WriteLine("Fatal error, can't properly connect to server.");
+                return;
+            }
+
+            // after you await the task, you have the status code & headers of the response, but not yet the response body
+            //response.Content.Headers.ContentType == "application/json";
+            //string json = await response.Content.ReadAsStringAsync();
+            //var rounds = JsonSerializer.Deserialize<List<Round>>(json);
+
+            Console.WriteLine(Game.Summary(name, rounds));
 
             //RandomMoveDecider moveDecider = new RandomMoveDecider();
             //Game game = new(name, moveDecider, repository);
