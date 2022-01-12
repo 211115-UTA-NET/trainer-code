5cb9796d65030dcd5f926115823f45d02138e519
Richard Hawkins <Richard.Hawkins@revature.com>
2022-01-12 15:31:19 -0500

    javascript example from batch

---
+++ wk7/JavaScript_Example/JavaScriptExample.js
@@ -0,0 +1,73 @@
+'use strict'
+
+// var myName = 'Tommy', age = 21, message = "hello";
+// var 5windows 
+// var MyName != myName
+// const birthday = '19.01.1991';
+// console.log(birthday);
+
+
+// let john = new user();
+
+// let user = 
+// {
+//     name = 'john',
+//     age = 30
+// };
+
+
+// var test = '33';
+
+
+// console.log("hi there, i'm " + test);
+
+// var result = 6
+
+
+
+//console.log(result ** 2 == 36)
+
+
+
+// = , == , ===
+
+
+// = is for assignment of a variable
+// == comparison opperator of the values
+// === is a comparison of the variable TYPE
+
+
+// let a  = 1; //value assignment
+
+// let b = 1;
+
+// // var result = (a == b); //value comparison
+
+// // console.log(result);
+ 
+// var result = ("1" === 1); //type comparison
+
+// console.log(result);
+
+// result = (1 === 1); //type comparison
+
+// console.log(result);
+
+// result = (a === b); //type comparison
+
+// console.log(result);
+
+
+
+
+var sending = 2;
+
+
+function newFunc(sent)
+{
+    console.log("hi there, you sent " + sent);
+    return "success"
+}
+
+var result = newFunc(sending);
+console.log( result)
