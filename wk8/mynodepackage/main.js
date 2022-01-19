c65112acddc319dd3d02e70ef839223c7e4c5847
Nicholas Escalona <nicholas.escalona@gmail.com>
2022-01-19 10:27:17 -0600

    tsconfig

--- wk8/mynodepackage/main.js
+++ wk8/mynodepackage/main.js
@@ -1,11 +1,15 @@
-let data = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.output = void 0;
+var data = {
     name: '123',
-    convert: () => '-123-'
+    convert: function () { return '-123-'; }
 };
 function output(something) {
     return something.convert().length;
 }
-let result = output(data);
-window.onload = () => {
-    document.body.innerHTML = `from typescript ${result}`;
+exports.output = output;
+var result = output(data);
+window.onload = function () {
+    document.body.innerHTML = "from typescript ".concat(result);
 };
