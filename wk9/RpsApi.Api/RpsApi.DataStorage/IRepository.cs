81e830c08a3b5fe3499bd13c7d3165060653c758
Nicholas Escalona <nicholas.escalona@gmail.com>
2022-01-26 10:36:04 -0600

    copy api and add dotnet tool and ef pkgs

---
+++ wk9/RpsApi.Api/RpsApi.DataStorage/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using RpsApi.Logic;
+
+namespace RpsApi.DataStorage
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<Round>> GetAllRoundsOfPlayerAsync(string name);
+        void AddNewRound(string? player1, string? player2, Round round);
+
+        Task<bool> PlayerExistsAsync(string player);
+
+        //void Save();
+    }
+}
