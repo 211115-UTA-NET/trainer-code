7d3642562863068b763f344012f65b8e98814f03
Nicholas Escalona <nicholas.escalona@gmail.com>
2022-01-26 11:59:01 -0600

    bad version of jain

--- wk9/RpsApi.Api/RpsApi.DataStorage/EfRepository.cs
+++ wk9/RpsApi.Api/RpsApi.DataStorage/EfRepository.cs
@@ -24,10 +24,24 @@ namespace RpsApi.DataStorage
         public async Task<IEnumerable<Logic.Round>> GetAllRoundsOfPlayerAsync(string name)
         {
             //_logger.LogInformation($"database connection test: {(await _context.Players.FirstAsync()).Name}");
+            //await PlayerExistsAsync(name);
 
-            await PlayerExistsAsync(name);
+            List<Model.Round> rounds = await _context.Rounds
+                .Where(r => r.Player1Navigation != null && r.Player1Navigation.Name == name)
+                .ToListAsync();
 
-            throw new NotImplementedException();
+            var neededMoveIds = rounds.SelectMany(r => new[] { r.Player1Move, r.Player2Move });
+            var neededMoves = _context.Moves.Where(m => neededMoveIds.Contains(m.Id));
+
+            // need to map from the EF model to the classes the consumers of this repository expect (Logic.Round)
+            // (otherwise we could just do "return rounds;")
+            return rounds.Select(r =>
+            {
+                // here, i have a string like "rock" and i want an enum value like Logic.Move.Rock
+                var m1 = (Logic.Move)Enum.Parse(typeof(Logic.Move), neededMoves.First(m => m.Id == r.Player1Move).Name);
+                var m2 = (Logic.Move)Enum.Parse(typeof(Logic.Move), neededMoves.First(m => m.Id == r.Player2Move).Name);
+                return new Logic.Round(r.Timestamp, m1, m2);
+            });
         }
 
         public void AddNewRound(string? player1, string? player2, Logic.Round round)
@@ -39,6 +53,9 @@ namespace RpsApi.DataStorage
         {
             //var x = player;
             //return await _context.Players.AnyAsync(p => p.Name == "nick') END; DROP TABLE Rps.Player;");
+
+            // EF automatically SQL-escapes any strings involved, preventing SQL injection.
+            // it also uses SqlParameters for any variables used in the query, further preventing SQL injection
             return await _context.Players.AnyAsync(p => p.Name == player);
             // little micro-optimization: if an async method's only "await" is right at the return statement
             //     then you can skip the await and the method doesn't need to be async (return the Task directly)
